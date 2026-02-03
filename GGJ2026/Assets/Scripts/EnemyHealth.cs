c1a9b93e3b6c253a0db694c554f5da9acac581f2
Beau Morgan <bmorgan0305@gmail.com>
2026-02-02 20:29:34 -0500

    Update EnemyHealth.cs

--- GGJ2026/Assets/Scripts/EnemyHealth.cs
+++ GGJ2026/Assets/Scripts/EnemyHealth.cs
@@ -1,6 +1,8 @@
 using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
+
 
 public class EnemyHealth : MonoBehaviour
 {
@@ -9,29 +11,130 @@ public class EnemyHealth : MonoBehaviour
     public Score score;
     int addScore = 25;
 
+    // Multi-material flash support
+    Renderer[] renderers;
+    Color[][] originalColors;
+
     void Start()
     {
         currentHealth = maxHealth;
+
+        renderers = GetComponentsInChildren<Renderer>();
+        originalColors = new Color[renderers.Length][];
+
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            Material[] mats = renderers[i].materials;
+            originalColors[i] = new Color[mats.Length];
+
+            for (int j = 0; j < mats.Length; j++)
+            {
+                originalColors[i][j] = mats[j].color;
+            }
+        }
     }
 
     public void TakeDamage(float amount)
     {
         currentHealth -= amount;
 
-        Debug.Log("Enemy Health"+currentHealth);
+        StartCoroutine(FlashRed());
 
         if (currentHealth <= 0)
         {
-            //score.score += addScore;
             Die();
+        }
+    }
 
+    IEnumerator FlashRed()
+    {
+        if (renderers == null || renderers.Length == 0) yield break;
+
+        // Flash red
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            Material[] mats = renderers[i].materials;
+            for (int j = 0; j < mats.Length; j++)
+            {
+                mats[j].color = Color.red;
+            }
+        }
+
+        yield return new WaitForSeconds(0.1f);
+
+        // Restore original colors
+        for (int i = 0; i < renderers.Length; i++)
+        {
+            Material[] mats = renderers[i].materials;
+            for (int j = 0; j < mats.Length; j++)
+            {
+                mats[j].color = originalColors[i][j];
+            }
         }
     }
 
     void Die()
     {
-        
         Destroy(gameObject);
-        //SceneManager.LoadScene("GameOverScreen");
     }
 }
+//public class EnemyHealth : MonoBehaviour
+//{
+//    public float maxHealth = 10f;
+//    public float currentHealth;
+//    public Score score;
+//    int addScore = 25;
+//    /// <summary>
+//    /// 
+//    /// </summary>
+//    Renderer Renderer;
+//    Color originalColor;
+//    /// <summary>
+//    /// 
+//    /// </summary>
+//    void Start()
+//    {
+//        currentHealth = maxHealth;
+
+
+
+
+//        enemyRenderer = GetComponentInChildren<Renderer>();
+//        originalColor = enemyRenderer.material.color;
+//    }
+
+//    public void TakeDamage(float amount)
+//    {
+//        currentHealth -= amount;
+//        ///
+//        StartCoroutine(FlashRed());
+//        ///
+//        Debug.Log("Enemy Health" + currentHealth);
+
+//        if (currentHealth <= 0)
+//        {
+//            //score.score += addScore;
+//            Die();
+
+//        }
+//    }
+
+//    void Die()
+//    {
+
+//        Destroy(gameObject);
+//        //SceneManager.LoadScene("GameOverScreen");
+//    }
+
+
+
+//    IEnumerator FlashRed()
+//    {
+//        if (enemyRenderer == null) yield break;
+
+//        enemyRenderer.material.color = Color.red;
+//        yield return new WaitForSeconds(0.1f);
+//        enemyRenderer.material.color = originalColor;
+//    }
+
+//}
